51cea8d1af3181ea424f6665f9327590d0aaa5b6
Dinuka_prasad <127348943+DinukaPrasad@users.noreply.github.com>
2026-03-18 11:44:20 +0000

    git initial commit

---
+++ src/main/java/com/attendly/attendly_backend/AttendlyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.attendly.attendly_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttendlyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AttendlyBackendApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/attendly/attendly_backend/modules/auth/AuthController.java
@@ -0,0 +1,9 @@
+package com.attendly.attendly_backend.modules.auth;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class AuthController {
+
+}
